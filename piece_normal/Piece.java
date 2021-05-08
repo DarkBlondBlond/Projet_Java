90c321de647e8a9eeed45f5827edd8beb9bf5660
Jerome_Kraft <74362305+DarkBlondBlond@users.noreply.github.com>
2021-05-08 17:25:20 +0200

    Add files via upload

--- piece_normal/Piece.java
+++ piece_normal/Piece.java
@@ -2,7 +2,36 @@
  * Classe principale Piece, avec les caractéristiques de bases communes à chaque pièce
  * A extends à chaque pièce.
  */
+
 public class Piece {
-    private ... ;
-    private ... ;
+    // nom pièce
+    private String nom = "";
+    // appartenance de la pièce au joueur 1 ou 2
+    private int joueur;
+
+    // constructeur :
+    public Piece(int j) {
+        this.joueur = j;
+    }
+
+    // méthodes getters :
+    public String getNom() {
+        return nom;
+    }
+
+    public int getJoueur() {
+        return joueur;
+    }
+
+    // Méthodes setters
+    public void setNom(String n) {
+        this.nom = n;
+    }
+
+    public void setJoueur(int j) {
+        this.joueur = j;
+    }
+
+    // Méthode déplacement :
+
 }
---
+++ piece_normal/Plateau.java
@@ -0,0 +1,6 @@
+/**
+ *  Classe Plateau pour créer un plateau de 9x9 cases (81 cases)
+ */
+public class Plateau {
+    
+}
